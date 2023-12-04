41f96f1866d317a8426aaa7832583a4da5fb27b8
Mike <mdowd4848@gmail.com>
2023-12-04 02:11:14 -0500

    smart_contract

--- server/server.py
+++ server/server.py
@@ -20,15 +20,65 @@ grid_size = 10
 # Load model
 model = PPO.load("../AI/models/model.zip")
 
-# deploy contract and load it
-contract_id, contract_address = deploy.deploy()
-account = beaker.localnet.get_accounts()[0]
-app_client = beaker.client.ApplicationClient(
-    client=beaker.localnet.get_algod_client(),
-    app=gamemanager.app,
-    signer=account.signer,
-    app_id=contract_id,
-)
+
+#gets current ship locations in gamemanager smart contract form
+def get_position_from_board(board_state):
+    pass
+
+
+@app.route('/setup_board', methods=['POST'])
+def setup_board():
+    data = request.json
+
+    if 'user_board' not in data or 'ai_board' not in data:
+        return jsonify({'error': 'Missing user_board or ai_board or both parameter'}), 400
+    
+    # deploy contract and load it
+    contract_id, contract_address = deploy.deploy()
+    account = beaker.localnet.get_accounts()[0]
+    app_client = beaker.client.ApplicationClient(
+        client=beaker.localnet.get_algod_client(),
+        app=gamemanager.app,
+        signer=account.signer,
+        app_id=contract_id,
+    )
+
+    user_board = data['user_board']
+    ai_board = data['ai_board']
+
+    user_pos, user_rot = get_pos_from_board(user_board)
+    ai_pos, ai_rot = get_pos_from_board(ai_board)
+
+
+    app_client.call(
+        gamemanager.new_game,
+        o_ship1_pos=user_pos[0],
+        o_ship1_rot=user_rot[0],
+        o_ship2_pos=user_pos[1],
+        o_ship2_rot=user_rot[1],
+        o_ship3_pos=user_pos[2],
+        o_ship3_rot=user_rot[2],
+        o_ship4_pos=user_pos[3],
+        o_ship4_rot=user_rot[3],
+        o_ship5_pos=user_pos[4],
+        o_ship5_rot=user_rot[4],
+    )
+
+    app_client.call(
+        gamemanager.submit_player_ship_positions,
+        p_ship1_pos=ai_pos[0],
+        p_ship1_rot=ai_rot[0],
+        p_ship2_pos=ai_pos[1],
+        p_ship2_rot=ai_rot[1],
+        p_ship3_pos=ai_pos[2],
+        p_ship3_rot=ai_rot[2],
+        p_ship4_pos=ai_pos[3],
+        p_ship4_rot=ai_rot[3],
+        p_ship5_pos=ai_pos[4],
+        p_ship5_rot=ai_rot[4],
+    )
+
+
 
 
 # Frontend must send post request with obs of current game state so that the model can make best move
